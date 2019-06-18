5f751625b0660f581312b2ff913e55ce319dd3cd
Beardless-sheik <44676055+Beardless-sheik@users.noreply.github.com>
2019-06-18 23:25:23 +0200

    Update print-date.js

--- print-date.js
+++ print-date.js
@@ -1,2 +1,10 @@
 // add a printDate function in here
-// eg: console.log("Hello World")
+function printDate(){
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateAndTime = date+' '+time;
+    console.log("The current date and time is: "+dateAndTime);
+}
+
+printDate();
