dd20a61b9e452345c17b25f69a6e43612eff403a
Beardless-sheik <44676055+Beardless-sheik@users.noreply.github.com>
2019-06-19 13:47:46 +0200

    Update calc-circumfrence.js

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,6 @@
 // calcCircumfrence function should be here
+function calcCircumfrence(radius){
+    var resultCirc=2*Math.PI*radius;
+    console.log("The circumference is: "+resultCirc);
+    console.log("The area of the circle is: "+resultCirc*resultCirc/(4*Math.PI));
+}
